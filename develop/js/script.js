8a0c89571054734f44dc9971c34d9ebfab87a370
Ryan Villanueva <ryanueva@gmail.com>
2022-12-20 17:11:51 -0500

    removed timeblocks from index.html, moved them to script.js

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,41 +1,50 @@
-// // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// // the code isn't run until the browser has finished rendering all the elements
-// // in the html.
-// $(function () {
-//   // TODO: Add a listener for click events on the save button. This code should
-//   // use the id in the containing time-block as a key to save the user input in
-//   // local storage. HINT: What does `this` reference in the click listener
-//   // function? How can DOM traversal be used to get the "hour-x" id of the
-//   // time-block containing the button that was clicked? How might the id be
-//   // useful when saving the description in local storage?
-//   //
-//   // TODO: Add code to apply the past, present, or future class to each time
-//   // block by comparing the id to the current hour. HINTS: How can the id
-//   // attribute of each time-block be used to conditionally add or remove the
-//   // past, present, and future classes? How can Day.js be used to get the
-//   // current hour in 24-hour time?
-//   //
-//   // TODO: Add code to get any user input that was saved in localStorage and set
-//   // the values of the corresponding textarea elements. HINT: How can the id
-//   // attribute of each time-block be used to do this?
-//   //
-//   // TODO: Add code to display the current date in the header of the page.
-// });
-
-function formatDate(dataObject) {
-  const parts = {
-    date: dateObject.getDate(),
-    month: dateObject.getMonth() + 1, 
-    year: dateObject.getFullYear(),
-    hour: dateObject.getHours(),
-    minute: dateObject.getMinutes().toString().padStart(2, "0"),
-    amOrPm: dateObject.getHours() < 12 ? "AM" : "PM"
-  };
-
-  return `${parts.date}/${parts.month}/${parts.year} ${parts.hour}:${parts.minute} ${parts.amOrPm}`;
-
-}
-
-const dayJsObject = dayjs();
-
-console.log(dayJsObject.format("DD/MM/YYYY hh:mm A"));
+  $(document).ready(function () {
+  //Holds all time slots (instead of index.html)
+  const timesOfWork = [
+    {
+      label: "9:00AM",
+      timeNumber: "9",
+      userInput: "",
+    },
+    {
+      label: "10:00AM",
+      timeNumber: "10",
+      userInput: "",
+    },
+    {
+      label: "11:00AM",
+      timeNumber: "11",
+      userInput: "",
+    },
+    {
+      label: "12:00PM",
+      timeNumber: "12",
+      userInput: "",
+    },
+    {
+      label: "01:00PM",
+      timeNumber: "13",
+      userInput: "",
+    },
+    {
+      label: "02:00PM",
+      timeNumber: "14",
+      userInput: "",
+    },
+    {
+      label: "03:00PM",
+      timeNumber: "15",
+      userInput: "",
+    },
+    {
+      label: "04:00PM",
+      timeNumber: "16",
+      userInput: "",
+    },
+    {
+      label: "05:00PM",
+      timeNumber: "17",
+      userInput: "",
+    },
+  ];
+});
